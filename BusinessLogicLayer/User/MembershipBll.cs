5ce924cc2f2fa2b6faed2ac08677a8ad970a1bb5
Elias Balmer <elias.balmer@bluewin.ch>
2013-11-17 23:19:36 +0100

    untracked files added

---
+++ BusinessLogicLayer/User/MembershipBll.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using EbalitWebForms.Common;
+using EbalitWebForms.DataLayer;
+using System.Web.Security;
+
+namespace EbalitWebForms.BusinessLogicLayer.User
+{
+    /// <summary>
+    /// Provides access to membership objects
+    /// </summary>
+    public class MembershipBll:DataAccessLayer
+    {
+        /// <summary>
+        /// Returns a list of all membership users
+        /// Only the username is set.
+        /// </summary>
+        /// <returns></returns>
+        public IList<aspnet_Users> GetUsers()
+        {
+            return Membership.GetAllUsers().Cast<MembershipUser>().ForEach(cc => new aspnet_Users
+            {
+                UserName = cc.UserName
+            });
+
+        }
+    }
+}
