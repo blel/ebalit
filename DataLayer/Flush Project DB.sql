1e46f4f6ca9f5918a24fa05f1c5e3bf09fcd8dd2
Elias Balmer <elias.balmer@bluewin.ch>
2013-12-09 21:22:24 +0100

    changes to flush script

--- DataLayer/Flush Project DB.sql
+++ DataLayer/Flush Project DB.sql
@@ -1,6 +1,7 @@
 delete from dbo.ProjectWorkingReports;
 delete from dbo.ProjectResourceTaskAssignments;
 delete from dbo.ProjectTasks;
-delete from dbo.ProjectResources;
+
 delete from dbo.ProjectUserAssignments;
+delete from dbo.ProjectResources;
 delete from dbo.ProjectProjects;
